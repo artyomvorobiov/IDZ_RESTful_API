67998bddbeaa29c0ff90352b48c7db2902a303fc
artyomvorobiov <89642304+artyomvorobiov@users.noreply.github.com>
2023-05-29 01:09:14 +0300

    Add files via upload

---
+++ SpringApi/src/test/java/ru/hse/springapi/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
