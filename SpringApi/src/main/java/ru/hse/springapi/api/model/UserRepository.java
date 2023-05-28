7e1523e01d4fb65351a41057d5d734d725d1363a
artyomvorobiov <89642304+artyomvorobiov@users.noreply.github.com>
2023-05-29 01:00:31 +0300

    Add files via upload

---
+++ SpringApi/src/main/java/ru/hse/springapi/api/model/UserRepository.java
@@ -0,0 +1,10 @@
+package ru.hse.springapi.api.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
