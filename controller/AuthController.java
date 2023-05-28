9d5e69b717513ec5f23bad53d3dc535b6d8dff56
artyomvorobiov <89642304+artyomvorobiov@users.noreply.github.com>
2023-05-29 01:10:09 +0300

    Add files via upload

---
+++ controller/AuthController.java
@@ -0,0 +1,37 @@
+package ru.hse.springapi.api.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hse.springapi.api.model.User;
+import ru.hse.springapi.api.model.UserRepository;
+import ru.hse.springapi.api.secure.MakeToken;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired private UserRepository userRepository;
+    @Autowired private MakeToken jwtUtil;
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/registration")
+    public Map<String, Object> registerHandler(@RequestBody User user){
+        String encodedPass = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPass);
+        if (user.getEmail().contains("@")) {
+            user = userRepository.save(user);
+            String token = jwtUtil.generateToken(user.getEmail());
+            return Collections.singletonMap("Ваш токен", token);
+        } else {
+            return Collections.singletonMap("Неправильный email",user.getEmail() );
+        }
+    }
+
+
+}
